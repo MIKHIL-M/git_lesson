e824e3c1d401a6ce8e3e9a3dc497439f25c86f0a
MIKHIL-M <mighilmurali@gmail.com>
2019-09-16 11:21:38 +0530

    x -> z and square 24

--- src/my_square.py
+++ src/my_square.py
@@ -1,8 +1,8 @@
-def my_square(x):
+def my_square(z):
 	"""It will takes a vaue and return the squre	
 	
 	uses the ** operator
 	"""
-	return(x ** 2)
+	return(z * z)
 
-print(my_square(4))
+print(my_square(24))
